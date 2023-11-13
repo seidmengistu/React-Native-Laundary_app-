8e4f3ccf88c9c3faa1173d8915fd147b2554da5f
seidmengistu <seidmengistu88@gmail.com>
2023-11-13 23:21:13 +0300

    Product detail page done

---
+++ screens/productDetails.style.js
@@ -0,0 +1,143 @@
+import { StyleSheet } from "react-native";
+import{COLORS,SIZES} from '../constants/index';
+
+const styles=StyleSheet.create({
+    container:{
+        flex: 1,
+        backgroundColor:COLORS.lightWhite
+    },
+
+    upperRow:{
+        marginHorizontal:20,
+        flexDirection: 'row',
+        justifyContent:'space-between',
+        alignItems: 'center',
+        position: 'absolute',
+        top: SIZES.xxLarge,
+        width: SIZES.width-44,
+        zIndex:999,
+
+    },
+    image:{
+    aspectRatio:1,
+    resizeMode: 'cover',
+    width:null,
+    height:null,
+
+    },
+    details:{
+        marginTop:-SIZES.large ,//to make the description on the top of the image
+        backgroundColor:COLORS.lightWhite,
+        width:SIZES.width,
+        borderTopLeftRadius:SIZES.medium,
+        borderTopRightRadius:SIZES.medium,
+    },
+
+    titleRow:{
+        marginHorizontal:20,
+        paddingBottom:SIZES.small,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        width:SIZES.width-44,
+        top:5,
+
+    },
+    title:{
+        fontFamily:'bold',
+        fontSize:SIZES.large,
+    },
+    price:{
+        paddingHorizontal:10,
+        fontFamily:'semibold',
+        fontSize:SIZES.large,
+        
+    },
+    priceWrapper:{
+        backgroundColor:COLORS.secondary,
+        borderRadius:SIZES.large,
+    },
+
+
+    ratingRow:{
+        paddingBottom:SIZES.small,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        width:SIZES.width-10,
+        top:20,
+
+    },
+    rating:{
+        top:SIZES.small-10,
+        flexDirection: 'row',
+        justifyContent:"flex-start",
+        alignItems:"center",
+        marginHorizontal:SIZES.large,
+
+    },
+    ratingText:{
+        color:COLORS.gray,
+        fontFamily:'medium',
+        paddingHorizontal:SIZES.xSmall,
+    },
+    descriptionWrapper:{
+        marginTop:SIZES.large*2,
+        marginHorizontal:SIZES.large
+    },
+    description:{
+        fontFamily:'medium',
+        fontSize:SIZES.large-2,
+
+    },
+    descText:{
+        fontFamily:'regular',
+        fontSize:SIZES.small,
+        textAlign:"justify",
+        marginBottom:SIZES.small,
+
+    },
+
+    location:{
+        flexDirection: 'row',
+        justifyContent:"space-between",
+        alignItems:"center",
+        backgroundColor:COLORS.secondary,
+        marginHorizontal:12,
+        padding:5,
+        borderRadius:SIZES.large,
+    },
+    cartRow:{
+        paddingBottom:SIZES.small,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        width:SIZES.width,
+    },
+    cartBtn:{
+        width:SIZES.width*0.7,
+        backgroundColor:COLORS.black,
+        padding:SIZES.small/2,
+        borderRadius:SIZES.large,
+        marginLeft:2
+    },
+    cartTitle:{
+        fontFamily:"bold",
+        fontSize:SIZES.medium,
+         color:COLORS.lightWhite,
+         marginLeft:SIZES.small,
+    },
+    addCart:{
+        width:37,
+        height:37,
+        borderRadius:50,
+        margin:SIZES.small,
+        backgroundColor:COLORS.black,
+        alignItems:"center",
+        justifyContent: 'center',
+    },
+   
+   
+});
+
+export default styles
