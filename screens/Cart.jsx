5a358ca759c5fdb836841b205df376bfd495207d
seidmengistu <seidmengistu88@gmail.com>
2023-11-13 13:35:22 +0300

    Cart doen

---
+++ screens/Cart.jsx
@@ -0,0 +1,12 @@
+import { View, Text,SafeAreaView } from 'react-native'
+import React from 'react'
+
+const Cart = () => {
+  return (
+    <SafeAreaViewView>
+      <Text>Cart</Text>
+    </SafeAreaViewView>
+  )
+}
+
+export default Cart
